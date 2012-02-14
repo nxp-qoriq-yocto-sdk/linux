12334bd568926d7c2274ce1043cde2d38e9547e4
Tang Yuantian <Yuantian.Tang@freescale.com>
2012-02-14 15:59:01 +0800

    powerpc/85xx: Add P1024rdb board support
    The p1024rdb has the similar feature as the p1020rdb. Therefore, p1024rdb use
    the same platform file as the p1/p2 rdb board.
    Overview of P2020RDB platform
    	- DDR3 1G
    	- NOR flash 16M
    	- 3 Ethernet interfaces
    	- NAND Flash 32M
    	- SPI EEPROM 16M
    	- SD/MMC
    	- 2 USB ports
    	- 4 TDM ports
    
    Signed-off-by: Jin Qing <b24347@freescale.com>
    Signed-off-by: Li Yang <leoli@freescale.com>
    Signed-off-by: Tang Yuantian <Yuantian.Tang@freescale.com>

--- arch/powerpc/platforms/85xx/mpc85xx_rdb.c
+++ arch/powerpc/platforms/85xx/mpc85xx_rdb.c
@@ -143,6 +143,7 @@ machine_device_initcall(p1020_rdb_pc, mpc85xxrdb_publish_devices);
 machine_device_initcall(p1020_utm_pc, mpc85xxrdb_publish_devices);
 machine_device_initcall(p1020_mbg_pc, mpc85xxrdb_publish_devices);
 machine_device_initcall(p1021_rdb_pc, mpc85xxrdb_publish_devices);
+machine_device_initcall(p1024_rdb, mpc85xxrdb_publish_devices);
 
 /*
  * Called very early, device-tree isn't unflattened
@@ -191,6 +192,13 @@ static int __init p1021_rdb_pc_probe(void)
 	unsigned long root = of_get_flat_dt_root();
 
 	return of_flat_dt_is_compatible(root, "fsl,P1021RDB-PC");
+}
+
+static int __init p1024_rdb_probe(void)
+{
+	unsigned long root = of_get_flat_dt_root();
+
+	return of_flat_dt_is_compatible(root, "fsl,P1024RDB");
 }
 
 define_machine(p2020_rdb) {
@@ -275,4 +283,18 @@ define_machine(p1021_rdb_pc) {
 	.restart                = fsl_rstcr_restart,
 	.calibrate_decr         = generic_calibrate_decr,
 	.progress               = udbg_progress,
+};
+
+define_machine(p1024_rdb) {
+	.name			= "P1024 RDB",
+	.probe			= p1024_rdb_probe,
+	.setup_arch		= mpc85xx_rdb_setup_arch,
+	.init_IRQ		= mpc85xx_rdb_pic_init,
+#ifdef CONFIG_PCI
+	.pcibios_fixup_bus	= fsl_pcibios_fixup_bus,
+#endif
+	.get_irq		= mpic_get_irq,
+	.restart		= fsl_rstcr_restart,
+	.calibrate_decr		= generic_calibrate_decr,
+	.progress		= udbg_progress,
 };
