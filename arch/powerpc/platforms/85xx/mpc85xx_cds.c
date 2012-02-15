3a6027e10512498b26a861187af535ffc11aac20
chenhui zhao <chenhui.zhao@freescale.com>
2012-02-15 16:08:10 +0000

    powerpc/mpc85xxcds: Add compatible entry "fsl, srio" to of_bus_ids[]
    Enable of_platform_bus_probe() to probe the RapidIO node in dts
    
    Signed-off-by: Zhao Chenhui <chenhui.zhao@freescale.com>
    Signed-off-by: Li Yang <leoli@freescale.com>

--- arch/powerpc/platforms/85xx/mpc85xx_cds.c
+++ arch/powerpc/platforms/85xx/mpc85xx_cds.c
@@ -390,6 +390,7 @@ static struct of_device_id __initdata of_bus_ids[] = {
 	{ .compatible = "soc", },
 	{ .compatible = "simple-bus", },
 	{ .compatible = "gianfar", },
+	{ .compatible = "fsl,srio", },
 	{},
 };
 
