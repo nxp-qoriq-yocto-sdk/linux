c14c71851c5b399dee198bffa7eb4678ee56b99f
Stuart Yoder <stuart.yoder@freescale.com>
2012-03-08 15:01:19 -0600

    Revert "KVM: PPC: bookehv: doorbell exit accounting"
    This reverts commit 963778bcad493ed10659d9a4242452665afb0e7e.

--- arch/powerpc/include/asm/kvm_host.h
+++ arch/powerpc/include/asm/kvm_host.h
@@ -84,10 +84,6 @@ struct kvm_vcpu_stat {
 	u32 dec_exits;
 	u32 ext_intr_exits;
 	u32 halt_wakeup;
-#ifdef CONFIG_KVM_BOOKE_HV
-	u32 dbell_exits;
-	u32 gdbell_exits;
-#endif
 #ifdef CONFIG_PPC_BOOK3S
 	u32 pf_storage;
 	u32 pf_instruc;
@@ -130,10 +126,6 @@ enum kvm_exit_types {
 	DEBUG_EXITS,
 	TIMEINGUEST,
 	PRG_INT_EXITS,
-#ifdef CONFIG_KVM_BOOKE_HV
-	DBELL_EXITS,
-	GDBELL_EXITS,
-#endif
 	__NUMBER_OF_KVM_EXIT_TYPES
 };
 
