163cec647b44349023c4d973f047e1d37a248346
Jerry Huang <Chang-Ming.Huang@freescale.com>
2012-03-14 17:08:27 +0800

    powerpc/85xx: add P1020MBG-PC platform support
    The p1020mbg-pc has the similar feature as the p1020rdb.
    Therefore, p1020mbg-pc use the same platform file as the p1/p2 rdb board.
    Overview of P1020MBG-PC platform:
            - DDR3 2GB
            - NOR flash 64MB
            - I2C EEPROM 256Kb
            - eTSEC1 (RGMII PHY) connected to VSC7385 L2 switch
            - eTSEC2 (SGMII PHY)
            - eTSEC3 (RGMII PHY)
            - SDHC
            - 2 USB ports
            - 4 TDM ports
            - PCIe (Lane1 to dual SATA controller)
    
    Signed-off-by: Jerry Huang <Chang-Ming.Huang@freescale.com>
    Signed-off-by: Kumar Gala <galak@kernel.crashing.org>

--- arch/powerpc/platforms/85xx/mpc85xx_rdb.c
+++ arch/powerpc/platforms/85xx/mpc85xx_rdb.c
@@ -131,6 +131,7 @@ static int __init mpc85xxrdb_publish_devices(void)
 machine_device_initcall(p2020_rdb, mpc85xxrdb_publish_devices);
 machine_device_initcall(p1020_rdb, mpc85xxrdb_publish_devices);
 machine_device_initcall(p1020_rdb_pc, mpc85xxrdb_publish_devices);
+machine_device_initcall(p1020_mbg_pc, mpc85xxrdb_publish_devices);
 
 /*
  * Called very early, device-tree isn't unflattened
@@ -158,6 +159,13 @@ static int __init p1020_rdb_pc_probe(void)
 	unsigned long root = of_get_flat_dt_root();
 
 	return of_flat_dt_is_compatible(root, "fsl,P1020RDB-PC");
+}
+
+static int __init p1020_mbg_pc_probe(void)
+{
+	unsigned long root = of_get_flat_dt_root();
+
+	return of_flat_dt_is_compatible(root, "fsl,P1020MBG-PC");
 }
 
 define_machine(p2020_rdb) {
@@ -191,6 +199,20 @@ define_machine(p1020_rdb) {
 define_machine(p1020_rdb_pc) {
 	.name			= "P1020RDB-PC",
 	.probe			= p1020_rdb_pc_probe,
+	.setup_arch		= mpc85xx_rdb_setup_arch,
+	.init_IRQ		= mpc85xx_rdb_pic_init,
+#ifdef CONFIG_PCI
+	.pcibios_fixup_bus	= fsl_pcibios_fixup_bus,
+#endif
+	.get_irq		= mpic_get_irq,
+	.restart		= fsl_rstcr_restart,
+	.calibrate_decr		= generic_calibrate_decr,
+	.progress		= udbg_progress,
+};
+
+define_machine(p1020_mbg_pc) {
+	.name			= "P1020 MBG-PC",
+	.probe			= p1020_mbg_pc_probe,
 	.setup_arch		= mpc85xx_rdb_setup_arch,
 	.init_IRQ		= mpc85xx_rdb_pic_init,
 #ifdef CONFIG_PCI
