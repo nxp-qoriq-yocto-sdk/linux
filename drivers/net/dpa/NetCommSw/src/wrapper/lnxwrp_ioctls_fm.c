b5b355c9d14c9eb2a0b32a2b6cdbb4c3e7254753
Andrei Pistirica <sorin.pistirica@freescale.com>
2012-05-24 11:59:43 +0300

    FMD IOCTL bad management solved.
    The following IOCTL calls weren't properly guarded:
    FM_PCD_IOC_MANIP_SET_NODE
    FM_PCD_IOC_MANIP_DELETE_NODE
    FM_PCD_IOC_STATISTICS_SET_NODE
    
    Signed-off-by: Andrei Pistirica <sorin.pistirica@freescale.com>

--- drivers/net/dpa/NetCommSw/src/wrapper/lnxwrp_ioctls_fm.c
+++ drivers/net/dpa/NetCommSw/src/wrapper/lnxwrp_ioctls_fm.c
@@ -2090,9 +2090,8 @@ invalid_port_id:
 
 #if defined(CONFIG_COMPAT)
         case FM_PCD_IOC_MANIP_SET_NODE_COMPAT:
-#else
-        case FM_PCD_IOC_MANIP_SET_NODE:
 #endif
+        case FM_PCD_IOC_MANIP_SET_NODE:
         {
             ioc_fm_pcd_manip_params_t *param;
 
@@ -2183,9 +2182,8 @@ invalid_port_id:
 
 #if defined(CONFIG_COMPAT)
         case FM_PCD_IOC_MANIP_DELETE_NODE_COMPAT:
-#else
-        case FM_PCD_IOC_MANIP_DELETE_NODE:
 #endif
+        case FM_PCD_IOC_MANIP_DELETE_NODE:
         {
             ioc_fm_obj_t id;
 
@@ -2214,12 +2212,11 @@ invalid_port_id:
 #ifdef FM_CAPWAP_SUPPORT
 #if defined(CONFIG_COMPAT)
         case FM_PCD_IOC_STATISTICS_SET_NODE_COMPAT:
-#else
-        case FM_PCD_IOC_STATISTICS_SET_NODE:
 #endif
+        case FM_PCD_IOC_STATISTICS_SET_NODE:
         {
             ioc_fm_pcd_stats_params_t param;
-#warning "TODO"
+#warning "feature not supported!"
             param->id = FM_PCD_StatisticsSetNode(p_LnxWrpFmDev->h_PcdDev,
                                 (t_FmPcdStatsParams *)&param);
         }
