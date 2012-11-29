85e57dbccac7fed24f77633913baa3f5ce325e1b
Zhao Chenhui <chenhui.zhao@freescale.com>
2012-11-29 18:50:29 +0800

    powerpc/85xx: fix compile error when CONFIG_HOTPLUG_CPU is disabled
    ../arch/powerpc/platforms/85xx/smp.c:306:4:
    error: implicit declaration of function 'generic_set_cpu_up'
    ../arch/powerpc/platforms/85xx/smp.c:321:4:
    error: implicit declaration of function 'is_core_down'
    make[3]: *** [arch/powerpc/platforms/85xx/smp.o] Error 1
    
    Signed-off-by: Zhao Chenhui <chenhui.zhao@freescale.com>
    Signed-off-by: Andy Fleming <afleming@freescale.com>

--- arch/powerpc/platforms/85xx/smp.c
+++ arch/powerpc/platforms/85xx/smp.c
@@ -316,6 +316,7 @@ static int __cpuinit smp_85xx_kick_cpu(int nr)
 		}
 	}
 
+#ifdef CONFIG_HOTPLUG_CPU
 	/* Starting Thread 0 will reset core, so put both threads down first */
 	if (smt_capable() && system_state == SYSTEM_RUNNING &&
 			cpu_thread_in_core(nr) == 0 && !is_core_down(nr)) {
@@ -323,6 +324,7 @@ static int __cpuinit smp_85xx_kick_cpu(int nr)
 				__func__, nr, cpu_last_thread_sibling(nr));
 			return -ENOENT;
 	}
+#endif
 #endif
 
 	np = of_get_cpu_node(nr, NULL);
